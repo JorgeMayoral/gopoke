8cea681492bd1ab8929833fee7aa88f5dab21b41
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-08-15 13:52:35 +0200

    feat: :sparkles: Add profiling code to main

--- src/cmd/gopoke/main.go
+++ src/cmd/gopoke/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	_ "flag"
-
-	"github.com/JorgeMayoral/gopoke/src/internal/fetching"
-	"github.com/JorgeMayoral/gopoke/src/internal/storage/pokeapi"
+	//"os"
+	//"runtime/pprof"
 
 	gopoke "github.com/JorgeMayoral/gopoke/src/internal"
 	"github.com/JorgeMayoral/gopoke/src/internal/cli"
+	"github.com/JorgeMayoral/gopoke/src/internal/fetching"
+	pokeapi "github.com/JorgeMayoral/gopoke/src/internal/storage/pokeapi"
+
 	"github.com/spf13/cobra"
 )
 
 func main() {
+	// CPU profiling code starts here
+	// f, _ := os.Create("pokemons.cpu.prof")
+	// defer f.Close()
+	// pprof.StartCPUProfile(f)
+	// defer pprof.StopCPUProfile()
+	// CPU profiling code ends here
+
 	var repo gopoke.PokemonRepo = pokeapi.NewPokeapiRepository()
 
 	fetchingService := fetching.NewService(repo)
@@ -19,4 +27,10 @@ func main() {
 	rootCmd := &cobra.Command{Use: "gopoke"}
 	rootCmd.AddCommand(cli.InitPokemonsCmd(fetchingService))
 	rootCmd.Execute()
+
+	// Memory profiling code starts here
+	// f2, _ := os.Create("pokemons.mem.prof")
+	// defer f2.Close()
+	// pprof.WriteHeapProfile(f2)
+	// Memory profiling code ends here
 }
