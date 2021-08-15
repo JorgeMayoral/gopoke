170dc9c6f3eb69644824229a87db7a9f1fa870df
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-08-15 12:42:25 +0200

    test: :white_check_mark: Update test with data provider

--- src/internal/fetching/service_test.go
+++ src/internal/fetching/service_test.go
@@ -1,6 +1,7 @@
 package fetching_test
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/JorgeMayoral/gopoke/src/internal/fetching"
@@ -8,16 +9,31 @@ import (
 )
 
 func TestFetchById(t *testing.T) {
+	tests := map[string]struct {
+		input int
+		want  int
+		err   error
+	}{
+		"valid pokemon":     {input: 25, want: 25, err: nil},
+		"not found pokemon": {input: 999999, err: errors.New("error")},
+	}
 	repo := inmem.NewRepository()
 	service := fetching.NewService(repo)
 
-	expected := 25
-	b, err := service.FetchByID(expected)
-	if err != nil {
-		t.Fatalf("expected %d, got an error %v", expected, err)
-	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			b, err := service.FetchByID(tc.input)
+			if err != nil && tc.err == nil {
+				t.Fatalf("not expected any errors and got %v", err)
+			}
 
-	if b.PokemonID != expected {
-		t.Fatalf("expected %d, got: %d", expected, b.PokemonID)
+			if err == nil && tc.err != nil {
+				t.Error("expected an error and got nil")
+			}
+
+			if b.PokemonID != tc.want {
+				t.Fatalf("expected %d, got: %d", tc.want, b.PokemonID)
+			}
+		})
 	}
 }
